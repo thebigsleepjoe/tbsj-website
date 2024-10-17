2794479f9fd44cdb25907738fad02ab91ec0108e
thebigsleepjoe <github@bigjoe.dev>
2024-10-17 02:42:19 -0700

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
+
+export const metadata: Metadata = {
+  title: "Joe",
+  description: "Joe's personal/portfolio website",
+};
+
+import { Navbar } from "./navbar";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/navbar.tsx
@@ -0,0 +1,42 @@
+import { faAddressBook, faBook, faCodeBranch, faHome, faMoon, IconDefinition } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export function NavbarItem({ href, icon, text }: { href: string; icon: IconDefinition, text: string }) {
+  return (
+    <a
+      href={href}
+      className="text-lg no-underline text-gray-400 hover:text-gray-100 ml-5"
+    >
+        <FontAwesomeIcon icon={icon} />
+        <span className="ml-2">{text}</span>
+    </a>
+  );
+}
+
+export function Navbar() {
+  return (
+    <div className="relative">
+        {/* Adjusted gradient overlay */}
+        <div className="absolute inset-x-0 bottom-0 h-10 bg-gradient-to-b from-gray-900/0 to-gray-900/50 pointer-events-none"></div>
+    
+        <nav className="bg-gradient-to-r from-gray-900 to-sky-950 font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 shadow sm:items-baseline w-full z-10">
+            <div className="mb-2 sm:mb-0">
+            <a
+                href="#"
+                className="text-2xl no-underline text-gray-400 hover:text-blue-dark"
+            >
+                <FontAwesomeIcon icon={faMoon} />
+                <span className="ml-2">bigsleepjoe.com</span>
+            </a>
+            </div>
+            <div>
+            <NavbarItem href="#" text="About" icon={faBook} />
+            <NavbarItem href="#" text="Contact" icon={faAddressBook} />
+            <NavbarItem href="https://www.github.com/thebigsleepjoe" text="GitHub" icon={faCodeBranch} />
+            </div>
+        </nav>
+    </div>
+
+
+  );
+}

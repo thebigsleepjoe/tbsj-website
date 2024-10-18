97ae2f70e6b346a7ceb8ee2f2f309dc341eb1766
thebigsleepjoe <github@bigjoe.dev>
2024-10-17 21:25:21 -0700

    Update Hero to utilize Tag element for a 'competencies' section in the hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Tag from "./tag";
 
 export function Hero() {
   return (
@@ -9,9 +10,32 @@ export function Hero() {
       <div className="absolute inset-0 bg-black opacity-50"></div>
     </div>
     
+    {/* The hero text in the left-center */}
     <div className="relative z-10 flex flex-col justify-center items-start h-full text-center ml-32">
       <h1 className="text-5xl font-bold leading-tight mb-4">@thebigsleepjoe</h1>
-      <p className="text-lg text-gray-300 mb-8">Software dev • Web dev • IT Support Specialist</p>
+      <p className="text-lg text-gray-100 mb-2">{'$'} Software Engineer • Web Developer • Technical Support</p>
+      <div className="flex flex-wrap gap-3 max-w-sm mt-3">
+        <Tag color="blue">React</Tag>
+        <Tag color="blue">TypeScript</Tag>
+        <Tag color="blue">Next.js</Tag>
+        <Tag color="yellow">JavaScript</Tag>
+        <Tag color="green">Python</Tag>
+        <Tag color="orange">HTML</Tag>
+        <Tag color="blue">CSS</Tag>
+        <Tag color="purple">Lua</Tag>
+        <Tag color="purple">Love2D</Tag>
+        <Tag color="green">AI</Tag>
+        <Tag color="green">Machine Learning</Tag>
+        <Tag color="green">LLMs</Tag>
+        <Tag color="red">Game Design</Tag>
+        <Tag color="red">Game Modding</Tag>
+        <Tag color="orange">Git</Tag>
+        <Tag color="orange">GNU/Linux</Tag>
+        <Tag color="teal">IoT</Tag>
+        <Tag color="blue">Windows</Tag>
+        <Tag color="indigo">Networking</Tag>
+        <Tag color="teal">Networking</Tag>
+      </div>
     </div>
 
     <div className="relative z-10 flex flex-col justify-end items-center h-full text-center">

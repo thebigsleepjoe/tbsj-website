4e194d9db5461efdfbd1b2e7c9c77c57b4af447a
thebigsleepjoe <github@bigjoe.dev>
2024-10-18 01:56:17 -0700

    Update Hero background again, center and resize Hero text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,18 @@ export function Hero() {
   return (
     <div className="-mt-16 -z-10 relative bg-gradient-to-r from-purple-600 to-blue-600 h-screen text-white overflow-hidden">
     <div className="-mt-16 -z-10 absolute inset-0">
-      <img src="https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?q=80&w=3648&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Background Image" className="object-cover object-center w-full h-full" />
-      <div className="absolute inset-0 bg-gradient-to-r from-purple-600 to-blue-600 opacity-75"></div>
+      <img src="https://images.unsplash.com/photo-1628114855639-f8294222fdc2?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Background Image" className="object-cover object-center w-full h-full" />
+      <div className="absolute inset-0 bg-gradient-to-tl from-purple-600 to-blue-600 opacity-75"></div>
       <div className="absolute inset-0 bg-black opacity-50"></div>
       {/* <div className="absolute inset-0 bg-gradient-to-t from-90% from-transparent to-gray-900"></div> */}
       <div className="absolute inset-0 bg-gradient-to-b from-80% from-transparent to-black opacity-40"></div>
       </div>
     
     {/* The hero text in the left-center */}
-    <div className="-mt-16 relative z-10 flex flex-col justify-center items-start h-full text-center ml-32">
-      <h1 className="text-8xl font-bold leading-tight mb-4">@thebigsleepjoe</h1>
-      <p className="text-3xl text-gray-100 mb-2">{'$'} Software Engineer • Web Developer • IT Specialist</p>
-      <div className="flex flex-wrap gap-3 max-w-xl mt-3">
+    <div className="-mt-16 relative z-10 flex flex-col justify-center items-center h-full text-center">
+      <h1 className="text-9xl font-bold leading-tight mb-4">@thebigsleepjoe</h1>
+      <p className="text-4xl text-gray-300 mb-2">{'$'} Software Engineer • Web Developer • IT Specialist</p>
+      <div className="flex flex-wrap gap-3 max-w-2xl mt-32">
         <Tag size='lg' color="blue">React</Tag>
         <Tag size='lg' color="blue">TypeScript</Tag>
         <Tag size='lg' color="blue">Next.js</Tag>

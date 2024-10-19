2be876a50725d9ab96e70c75b023038cc8f28414
thebigsleepjoe <github@bigjoe.dev>
2024-10-19 03:40:59 -0700

    Add pretty gradients to the Doc background

--- src/app/doc.tsx
+++ src/app/doc.tsx
@@ -17,8 +17,14 @@ export function Doc({ children }: DocProps) {
   return (
     <section className='w-full flex justify-center bg-gray-900'>
       <div className='w-full md:w-2/3 2xl:w-5/12'>
-        <div className='bg-transparent border md:border-2 border-gray-700 shadow-emerald-300 rounded-2xl p-8 md:p-12 2xl:p-20 w-full mt-16'>
-          {children}
+        <div className='bg-transparent relative border md:border-2 border-gray-700 shadow-emerald-300 rounded-2xl p-8 md:p-12 2xl:p-20 w-full mt-16'>
+          <div className='absolute rounded-2xl inset-0 bg-gradient-to-r from-transparent to-black opacity-50'/>
+          <div className='absolute rounded-2xl inset-0 bg-gradient-to-b from-transparent to-black opacity-50'/>
+          <div className='absolute rounded-2xl inset-0 bg-gradient-to-bl from-green-800 to-orange-500 opacity-5'/>
+          <div className='absolute rounded-2xl inset-0 bg-gradient-to-tl from-blue-800 to-purple-500 opacity-5'/>
+          <div className='relative z-10'>
+            {children}
+          </div>
         </div>
       </div>
     </section>

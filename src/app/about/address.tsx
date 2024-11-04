fedf1b897b9c4a1ec787f628ec0d637e4caf0bd7
thebigsleepjoe <github@bigjoe.dev>
2024-11-04 01:11:39 -0800

    Add component for obfuscating email addresses from web scrapers

---
+++ src/app/about/address.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { useEffect, useState } from 'react';
+
+interface ObfuscatedEmailProps {
+  user: string;
+  domain: string;
+  tld: string;
+}
+
+export default function ObfuscatedEmail({ user, domain, tld }: ObfuscatedEmailProps) {
+  const [email, setEmail] = useState<string>('');
+
+  useEffect(() => {
+    setEmail(`${user}@${domain}.${tld}`);
+  }, [user, domain, tld]);
+
+  if (!email) return null; // Prevent rendering until the email is set
+
+  return (
+    <a href={`mailto:${email}`} aria-label="Email address">
+      {email}
+    </a>
+  );
+}

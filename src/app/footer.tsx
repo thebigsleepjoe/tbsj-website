9080fb157feba4d14e448eccdcc4d98e82ff61e5
thebigsleepjoe <github@bigjoe.dev>
2024-11-06 01:36:13 -0800

    Update footer by adding additional row and comments, also be a little silly

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -1,4 +1,4 @@
-import { faCodeBranch, faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faBook, faCodeBranch, faEnvelope, faHeart, faHeartBroken, faMailBulk, faMailReply } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export function Footer() {
@@ -8,7 +8,7 @@ export function Footer() {
     <footer className='relative w-full bg-gradient-to-b from-black to-gray-950 border-t-2 border-gray-900'>
       <div className='absolute inset-0 bg-gradient-to-r from-transparent to-red-600 opacity-10 -z-10' />
       <div className='container mx-auto px-4 py-24 text-gray-500 text-lg md:text-2xl'>
-        {/* Columns */}
+        {/* Top row */}
         <div className='flex justify-center'>
           <div className='flex justify-between w-full max-w-3xl'>
             <p>
@@ -24,11 +24,25 @@ export function Footer() {
           </div>
         </div>
 
-        {/* Single column row */}
+        {/* Middle row */}
+        <div className='flex justify-center mt-2'>
+          <div className='flex justify-between w-full max-w-3xl'>
+            <p />
+            <a
+              href='/about#contact'
+              className='text-cyan-600 hover:text-cyan-400 transition-colors'
+            >
+              <FontAwesomeIcon icon={faEnvelope} />
+              <span className='ml-2'>Email</span>
+            </a>
+          </div>
+        </div>
+
+        {/* Bottom row */}
         <div className="flex justify-center w-full mt-16">
           <div>
             <span className='mr-2'>Made by Joe, with</span>
-            <FontAwesomeIcon icon={faHeart} className='text-red-600 hover:animate-spin' />
+            <FontAwesomeIcon icon={faHeartBroken} className='text-red-600 hover:animate-spin' />
           </div>
         </div>
       </div>
